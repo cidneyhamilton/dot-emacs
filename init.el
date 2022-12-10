c190aed49c26f27d0118b3a5cfc210a8af263add
Cidney Hamilton <cidney@cidneyhamilton.com>
2022-12-10 09:55:35 -0500

    Update init.el

--- init.el
+++ init.el
@@ -39,6 +39,10 @@
 (use-package helm
   :straight t)
 
+;; (global-set-key (kbd "M-x") 'helm-M-x)
+;; (global-set-key (kbd "C-x C-f") 'helm-find-files)
+;; (global-set-key (kbd "C-x b") 'helm-buffers-list)
+
 (add-to-list 'auto-mode-alist '("\\.html.erb" . html-erb-mode))
 
 (defmacro my/hook (package-name hook &rest body)
@@ -91,6 +95,12 @@ PACKAGE-NAME is a unique prefix given to each function hook name."
 ;; Load mail configuration
 (load-file "~/.emacs.d/mail.el")
 
+;; Google Translate
+(setq google-translate-default-source-language "de")
+(setq google-translate-default-target-language "en")
+(global-set-key "\C-ct" 'google-translate-at-point)
+(global-set-key "\C-cT" 'google-translate-query-translate)
+
 ;; Load IRC configuration
 (setq rcirc-default-nick "cidney")
 (setq erc-server "irc.libera.chat"
@@ -122,17 +132,47 @@ PACKAGE-NAME is a unique prefix given to each function hook name."
 (when (eq system-type 'gnu/linux)
   (setenv "FrameworkPathOverride" "/lib/mono/4.5"))
 
+;; Godot
+(use-package gdscript-mode
+    :straight (gdscript-mode
+               :type git
+               :host github
+               :repo "godotengine/emacs-gdscript-mode"))
+
+(setq gdscript-godot-executable "~/Dev/Godot/Godot_v3.5.1-stable_x11.64")
+
+;; LSP mode fix
+(defun lsp--gdscript-ignore-errors (original-function &rest args)
+  "Ignore the error message resulting from Godot not replying to the `JSONRPC' request."
+  (if (string-equal major-mode "gdscript-mode")
+      (let ((json-data (nth 0 args)))
+        (if (and (string= (gethash "jsonrpc" json-data "") "2.0")
+                 (not (gethash "id" json-data nil))
+                 (not (gethash "method" json-data nil)))
+            nil ; (message "Method not found")
+          (apply original-function args)))
+    (apply original-function args)))
+;; Runs the function `lsp--gdscript-ignore-errors` around `lsp--get-message-type` to suppress unknown notification errors.
+(advice-add #'lsp--get-message-type :around #'lsp--gdscript-ignore-errors)
 
 ;; Org mode setup
 (require 'org)
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
-(setq org-todo-keywords '("TODO" "STARTED" "WAITING" "DONE"))
+(setq org-capture-templates
+      '(("d" "Distraction" entry (file+headline "~/org/distractions.org" "Distractions") "* %?\n%T")
+        ("i" "Inbox" entry (file+headline "~/org/inbox.org" "Inbox") "* %?\n%T")))
+
+(setq org-todo-keywords '("TODO" "NEXT" "WAITING" "DONE"))
 (setq org-agenda-include-diary t)                                               
 (setq org-agenda-include-all-todo t)
 (setq org-default-notes-file "~/org/inbox.org")
 (setq org-src-preserve-indentation nil)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((python . t)))
 
 ;; LSP
 (use-package lsp-mode
@@ -144,6 +184,10 @@ PACKAGE-NAME is a unique prefix given to each function hook name."
 
 ;; (require 'dap-unity)
 
+;; Mastodon
+(setq mastodon-instance-url "https://social.city-of-glass.net"
+      mastodon-active-user "cidney")
+
 ;; CSHARP
 (use-package csharp-mode
   :straight t
@@ -154,6 +198,51 @@ PACKAGE-NAME is a unique prefix given to each function hook name."
     (c-toggle-auto-newline)
     (gsetq-local c-basic-offset 4)))
 
+;; Elfeed
+(setq elfeed-feeds
+      '(
+        ("https://www.reddit.com/r/writing.rss" writing reddit)        
+        ("https://www.reddit.com/r/emacs.rss" emacs reddit)
+        ("https://www.reddit.com/r/German.rss" german reddit)
+        ("https://www.reddit.com/r/gamedev.rss" gamedev reddit)
+        ("https://erzadel.net/feed.xml" people)
+        ("https://rusingh.com/feed/" people webdev)
+        ("https://christine.website/blog.rss" webdev)
+        ("https://deadsuperhero.com/rss/" people)
+        ("https://nolanlawson.com/feed" webdev)
+        ("https://www.brainonfire.net/blog/" people webdev)
+        ("http://cidney.org/feed.xml" people)
+        ("https://www.brainonfire.net/blog/posts.atom" people webdev)
+        ("https://babbagefiles.xyz/posts/index.xml" people webdev)
+        ("https://blogghoran.se/feed" people)
+        ("https://passionandsoul.com/blog/feed" people)
+        ("https://theundercoverintrovert.com/feed" writing)
+        ("https://cygnusentertainment.com/blog/feed" gamedev)
+        ("https://amandapalmer.net/posts/feed" people)
+        ("https://www.neilgaiman.com/feed/journal/" people writing)
+        ("https://blindjournalist.wordpress.com/rss" people writing)
+        ("https://jekyllrb.com/feed.xml" webdev)
+        ("https://www.inklestudios.com/blog/" gamedev)
+        ("https://laurakalbag.com/posts/index.xml" webdev)
+        ("https://ar.al/index.xml" webddev)
+        ("https://emshort.blog/feed" gamedev)
+        ("https://ben304.blogspot.com/feeds/posts/default?alt=rss" gamedev)
+        ("https://www.gibberlings3.net/rss/1-infinity-engine-modding-news.xml" gamedev)
+        ("https://www.baldurbjarnason.com/feed.xml" webdev)
+        ("https://drewdevault.com/blog/index.xml" webdev)
+        ("http://jeffmachwrites.com/rss" writing people)
+        ("https://godotengine.org/rss.xml" gamdev)
+        ("https://victoriacorva.xyz/feed" writing)
+        ("http://decafbad.net/feed/index.xml" people)
+        ("https://alexschroeder.ch/wiki/feed/full" people emacs)
+        ("https://pluralistic.net/rss" news)
+        ("https://craphound.com/feed" news)
+        ("http://pedestrianobservations.com/feed" people)
+        ("https://sachachua.com/blog/feed" emacs)
+        ("https://abagond.wordpress.com/feed" people)
+        )
+      )
+
 ;; Unity
 (use-package unity
   :straight (unity :type git :host github :protocol ssh
@@ -163,14 +252,6 @@ PACKAGE-NAME is a unique prefix given to each function hook name."
   (unity-build-code-shim)
   (unity-setup))
 
-;; Music
-(load-file "~/.emacs.d/emms.el")
-
-;; JSX mode
-(load-file "~/.emacs.d/jsx-mode.el")
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
-(autoload 'jsx-mode "jsx-mode" "JSX mode" t)
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -178,9 +259,8 @@ PACKAGE-NAME is a unique prefix given to each function hook name."
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" default))
- '(elfeed-feeds
-   '("https://erzadel.net/feed.xml" "https://rusingh.com/feed/" "https://christine.website/blog.rss" "https://deadsuperhero.com/rss/" "https://anchor.fm/s/52f2db0c/podcast/rss" "https://nolanlawson.com/feed" "https://nolanlawson.com/" "https://www.brainonfire.net/blog/" "http://cidney.org/feed.xml" "https://www.brainonfire.net/blog/posts.atom" "https://babbagefiles.xyz/posts/index.xml" "https://whatthefuckjusthappenedtoday.com/rss.xml" "https://heathermeeker.com/rss" "https://samkriss.com/rss" "https://blogghoran.se/feed" "https://itsfoss.com/feed/" "passionandsoul.com/blog/feed" "https://theundercoverintrovert.com/feed" "cygnusentertainment.com/blog/feed" "https://amandapalmer.net/posts/feed" "https://www.neilgaiman.com/feed/journal/" "https://blindjournalist.wordpress.com/rss" "https://jekyllrb.com/feed.xml" "https://www.inklestudios.com/blog/" "https://laurakalbag.com/posts/index.xml" "https://ar.al/index.xml" "https://www.youtube.com/feeds/videos.xml?channel_id=UCnPM1kSZf91ZGkcgy95Q" "https://kensgame.com/blog/feed" "https://emshort.blog/feed" "https://ben304.blogspot.com/feeds/posts/default?alt=rss" "https://blog.unity.com/technology/speed-up-your-programmer-workflows" "https://blog.unity.com/feed" "https://www.gibberlings3.net/rss/1-infinity-engine-modding-news.xml/" "https://www.baldurbjarnason.com/feed.xml" "https://drewdevault.com/blog/index.xml" "http://jeffmachwrites.com/rss" "https://godotengine.org/rss.xml" "https://blog.unity.com/rss-feeds" "https://victoriacorva.xyz/feed" "http://decafbad.net/feed/index.xml" "https://alexschroeder.ch/wiki/feed/full" "https://pluralistic.net/rss" "https://craphound.com/feed" "http://pedestrianobservations.com/feed" "https://sachachua.com/blog/feed" "https://abagond.wordpress.com/feed"))
- '(org-agenda-files '("~/org/gtd.org"))
+ '(ispell-dictionary nil)
+ '(org-agenda-files nil)
  '(org-log-into-drawer t)
  '(package-enable-at-startup nil)
  '(package-selected-packages
