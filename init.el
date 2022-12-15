e36131b938b6635aa6e6c8fa356eaf4d9a21b5c9
Cidney Hamilton <cidney@cidneyhamilton.com>
2022-12-15 13:55:12 -0500

    Update config.

--- init.el
+++ init.el
@@ -19,7 +19,7 @@
 (add-to-list 'auto-mode-alist '("\\.html.erb" . html-erb-mode))
 
 ;; tab-width
-(setq-default tab-width 1)
+(setq-default tab-width 2)
 
 ;; Display options
 (use-package solarized-theme
@@ -61,18 +61,20 @@
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
+(setq org-src-tabs-act-natively t)
 (setq org-capture-templates
       '(("d" "Distraction" entry (file+headline "~/org/distractions.org" "Distractions") "* %?\n%T")
         ("i" "Inbox" entry (file+headline "~/org/inbox.org" "Inbox") "* %?\n%T")))
 
-(setq org-todo-keywords '("TODO" "NEXT" "WAITING" "DONE"))
+(setq org-todo-keywords '("TODO" "NEXT" "WAITING" "MAYBE" "DONE"))
 (setq org-agenda-include-all-todo t)
 (setq org-default-notes-file "~/org/inbox.org")
 (setq org-src-preserve-indentation nil)
 
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((python . t)))
+ '((python . t)
+	 (ruby . t)))
 
 ;; LSP
 ;; (use-package lsp-mode
@@ -92,43 +94,38 @@
 ;; Elfeed
 (setq elfeed-feeds
       '(
-        ("https://www.reddit.com/r/writing.rss" writing reddit)        
-        ("https://www.reddit.com/r/emacs.rss" emacs reddit)
-        ("https://www.reddit.com/r/German.rss" german reddit)
-        ("https://www.reddit.com/r/gamedev.rss" gamedev reddit)
-        ("https://erzadel.net/feed.xml" people)
-        ("https://rusingh.com/feed/" people webdev)
+        ("https://erzadel.net/feed.xml" people fediverse)
+        ("https://rusingh.com/feed/" people webdev fediverse)
         ("https://christine.website/blog.rss" webdev)
-        ("https://deadsuperhero.com/rss/" people)
-        ("https://nolanlawson.com/feed" webdev)
-        ("https://www.brainonfire.net/blog/" people webdev)
-        ("http://cidney.org/feed.xml" people)
-        ("https://www.brainonfire.net/blog/posts.atom" people webdev)
+        ("https://deadsuperhero.com/rss/" people gamedev)
+        ("https://nolanlawson.com/feed" people webdev)
+        ("https://cidney.org/feed.xml" people)
+        ("https://brainonfire.net/blog/posts.atom" people webdev)
         ("https://babbagefiles.xyz/posts/index.xml" people webdev)
-        ("https://blogghoran.se/feed" people)
+        ("https://blogghoran.se/feed" people fediverse)
         ("https://passionandsoul.com/blog/feed" people)
-        ("https://theundercoverintrovert.com/feed" writing)
+        ("https://theundercoverintrovert.com/feed" writing people)
         ("https://cygnusentertainment.com/blog/feed" gamedev)
         ("https://amandapalmer.net/posts/feed" people)
-        ("https://www.neilgaiman.com/feed/journal/" people writing)
+        ("https://neilgaiman.com/feed/journal/" people writing)
         ("https://blindjournalist.wordpress.com/rss" people writing)
         ("https://jekyllrb.com/feed.xml" webdev)
-        ("https://www.inklestudios.com/blog/" gamedev)
+        ("https://inklestudios.com/blog/" gamedev)
         ("https://laurakalbag.com/posts/index.xml" webdev)
         ("https://ar.al/index.xml" webddev)
         ("https://emshort.blog/feed" gamedev)
         ("https://ben304.blogspot.com/feeds/posts/default?alt=rss" gamedev)
-        ("https://www.gibberlings3.net/rss/1-infinity-engine-modding-news.xml" gamedev)
-        ("https://www.baldurbjarnason.com/feed.xml" webdev)
+        ("https://gibberlings3.net/rss/1-infinity-engine-modding-news.xml" gamedev)
+        ("https://baldurbjarnason.com/feed.xml" webdev)
         ("https://drewdevault.com/blog/index.xml" webdev)
-        ("http://jeffmachwrites.com/rss" writing people)
+        ("https://jeffmachwrites.com/rss" writing people)
         ("https://godotengine.org/rss.xml" gamdev)
         ("https://victoriacorva.xyz/feed" writing)
-        ("http://decafbad.net/feed/index.xml" people)
+        ("https://decafbad.net/feed/index.xml" people)
         ("https://alexschroeder.ch/wiki/feed/full" people emacs)
         ("https://pluralistic.net/rss" news)
         ("https://craphound.com/feed" news)
-        ("http://pedestrianobservations.com/feed" people)
+        ("https://pedestrianobservations.com/feed" people)
         ("https://sachachua.com/blog/feed" emacs)
         ("https://abagond.wordpress.com/feed" people)
         )
@@ -140,13 +137,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" default))
+			'("fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" default))
  '(ispell-dictionary nil)
- '(org-agenda-files nil)
+ '(org-agenda-files '("~/org/projects.org"))
  '(org-log-into-drawer t)
  '(package-enable-at-startup nil)
  '(package-selected-packages
-   '(jabber helm bitlbee org-jira mpv which-key omnisharp emms emacsql emacsql-sqlite lsp-mode magit markdown-mode elfeed csharp-mode solarized-theme elpher darkroom ink-mode)))
+			'(emms markdown-mode elfeed solarized-theme elpher darkroom ink-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
